8fd3e975a965e06a062802a8c33d05696896133a
Saeed Mohamed <sydalshghty15@gmail.com>
2026-04-28 13:55:51 +0300

    upload-all-new-changes

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ navigateSliderToShop();
 
 /*Categories Swiper-Slider*/
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3, // موبايل
+    initialSlide: 0,
+    slidesPerView: 2, // موبايل
     spaceBetween: 10,
+    slidesPerGroup: 2, // 👈 عدد السلايدات اللي تتحرك مع بعض
 
     breakpoints: {
-        576: { slidesPerView: 3 },   // موبايل كبير
-        768: { slidesPerView: 4 },   // تابلت
-        992: { slidesPerView: 6 },   // لابتوب
-        1200: { slidesPerView: 8 },  // ديسكتوب كبير
+        576: { slidesPerView: 3, slidesPerGroup: 3 },   // موبايل كبير
+        768: { slidesPerView: 4, slidesPerGroup: 4 },   // تابلت
+        992: { slidesPerView: 6, slidesPerGroup: 6 },   // لابتوب
+        1200: { slidesPerView: 8, slidesPerGroup: 8 },  // ديسكتوب كبير
     },
 
     navigation: {
